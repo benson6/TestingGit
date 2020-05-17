be5392ce18c24e0f23df46ec0a9d7c272773c06a
Benjamin Craig <benjamincraig@Benjamins-MacBook-Pro.local>
2020-05-17 20:53:32 +0100

    test push to two remotes

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
-app.get ('/', (req,res)=> res.send('Hello world x1000'))
+app.get ('/', (req,res)=> res.send('Hello world x2000'))
 
 app.listen(PORT, ()=>console.log(`example app listening on ${PORT}`))
+
+console.log("testing git");
+
 
 
 
