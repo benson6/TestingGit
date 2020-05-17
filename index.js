dcea8deb0a56d8d0e3b3e15e4ca09192ce313db5
Benjamin Craig <benjamincraig@Benjamins-MacBook-Pro.local>
2020-05-17 21:00:09 +0100

    second test route added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT;
 
 app.get ('/', (req,res)=> res.send('Hello world x2000'))
 
+app.get ('/sleep, (req, res)=> res.send('you have called the sleep route'))
+
 app.listen(PORT, ()=>console.log(`example app listening on ${PORT}`))
 
 console.log("testing git");
